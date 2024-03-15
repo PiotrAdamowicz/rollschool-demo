b9f6aa56a90de0c965ae1beef70e4038305b52f7
Piotr Adamowicz <adamowicz.piotr2@gmail.com>
2024-03-15 12:56:16 +0100

    styling: Hero RWD adjustment

---
+++ src/pages/metadata.ts
@@ -0,0 +1,44 @@
+export const pages = {
+  faq: {
+    siteTitle: "FAQ",
+    siteDescription: "Rollschool: często zadawane pytania",
+    content: []
+  },
+  home: {
+    siteTitle: "Rollschool",
+    siteDescription:
+      "Rollschool Szkoła jazdy na rolkach i wrotkach w Warszawie. Nauka jazdy na rolkach i wrotkach dla dzieci i dorosłych. Uczymy jeździć niezależnie od wieku.",
+    content: []
+  },
+  klubSportowy: {
+    siteTitle: "Klub Sportowy Rollschool",
+    siteDescription: "Klub Sportowy Rollschool",
+    content: []
+  },
+  kontakt: { siteTitle: "Kontakt", siteDescription: "Kontakt", content: [] },
+  bladecross: {
+    siteTitle: "MLB",
+    siteDescription: "Mazowiecka Liga Bladecross",
+    content: []
+  },
+  ofundacji: {
+    siteTitle: "O Fundacji",
+    siteDescription: "",
+    content: []
+  },
+  rolkowaMapa: {
+    siteTitle: "Rolkowa Mapa Warszawy",
+    siteDescription: "Rolkowa Mapa Warszawy",
+    content: []
+  },
+  sklep: {
+    siteTitle: "Sklep Rollschool",
+    siteDescription: "Sklep Rollschool",
+    content: []
+  },
+  wypozyczalnia: {
+    siteTitle: "Wypożyczalnia rolek",
+    siteDescription: "Wypożyczalnia rolek",
+    content: []
+  }
+};
